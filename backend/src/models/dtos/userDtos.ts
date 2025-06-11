e613b9f9ea2ce494137229844411bf629efdc326
Erik Windows10 Victor <erikgcoutinho03@gmail.com>
2025-06-10 23:45:27 -0300

    feat: Adicionado primeiros DTOs para usuários.

---
+++ backend/src/models/dtos/userDtos.ts
@@ -0,0 +1,44 @@
+export type CreateUserDTO = {
+	name: string;
+	ra: string;
+	email: string;
+	password: string;
+};
+export type UpdateUserNameDTO = {
+	ra?: string;
+	email?: string;
+	name: string;
+};
+export type UpdateUserPasswordDTO = {
+	ra?: string;
+	email?: string;
+	password: string;
+};
+export type UpdateUserEmailDTO = {
+	ra?: string;
+	oldEmail?: string;
+	newEmail: string;
+};
+export type UpdatedUserDTO = {
+	name: string;
+	ra: string;
+	email: string;
+    password?: string;
+};
+export type DeleteUserDTO = {
+	ra?: string;
+	email?: string;
+};
+
+export type SafeUserDTO = {
+    name: string;
+    ra: string;
+    email: string;
+}
+
+export type FullUserDTO = {
+	name: string;
+	ra: string;
+	email: string;
+	password: string;
+};
