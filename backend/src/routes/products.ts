3911fa6e7894526910f543349830b03b52f40b61
Gian <gian.2000@hotmail.com>
2025-06-16 18:23:34 -0300

    Update products.ts

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -1,25 +1,21 @@
 import { Router } from "express";
-import { EHttpStatusCode as httpResponses } from "../@types/httpStatusCode.js";
-export const router = Router({mergeParams:true});
+import { ProductController } from "../controllers/productController.js";
+import { ProductService } from "../models/services/productService.js";
+import { ProductRepository } from "../models/repositories/productRepository.js";
+import { IProductRepository } from "../models/repositories/IProductRepository.js";
 
-router.get("/",(req,res)=>{
-    //TODO: Implementar busca por todos os produtos no banco
-})
+export const router = Router({mergeParams: true});
 
-router.get("/id/:id", (req, res) => {
-  const { id } = req.params;
-  //TODO: Implementar busca por ID de usuário no banco. ProductsController.
-  res.status(httpResponses.OK).json({ message: "Product Found!", id });
-});
+// Instanciação das camadas de dependência
+const productRepository: IProductRepository = new ProductRepository();
+const productService = new ProductService(productRepository);
+const productController = new ProductController(productService);
 
-router.get("/name/:name", (req, res) => {
-  const { name } = req.params;
-  //TODO: Implementar busca por nome de usuário no banco. ProductsController.
-  res.status(httpResponses.OK).json({ message: "Product Found!", name });
-});
-
-router.get("/origin/:origin", (req, res) => {
-  const { origin } = req.params;
-  //TODO: Implementar busca por origem de usuário no banco. ProductsController.
-  res.status(httpResponses.OK).json({ message: "Product Found!", origin });
-});
+// Rotas de Produto
+router.post("/", productController.createProduct);
+router.get("/", productController.getAllProducts);
+router.get("/id/:id", productController.getProductById);
+router.get("/name/:name", productController.getProductByName);
+router.get("/origin/:origin", productController.getProductsByOrigin);
+router.put("/:id", productController.updateProduct); // Atualização por ID
+router.delete("/:id", productController.deleteProduct); // Deleção por ID
