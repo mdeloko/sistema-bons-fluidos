0d2b8470327b0d3bf02f44c7f80638b6c9b9f7db
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2025-06-17 01:34:32 -0300

    fix/feat: Atualizada a regra de Repositório para trabalhar com Entidades e não DTOs, além de troca de algumas lógicas para melhores implementações.

--- backend/src/models/repositories/userRepository.ts
+++ backend/src/models/repositories/userRepository.ts
@@ -1,44 +1,45 @@
 import DBConnection from "../../utils/db.js";
 import { CreateUserDTO, DeleteUserDTO, FullUserDTO, SafeUserDTO, UpdateUserEmailDTO, UpdateUserNameDTO, UpdateUserPasswordDTO } from "../dtos/userDtos.js";
 import {QueryResult,QueryResultRow,DatabaseError} from "pg";
+import { User } from "../entities/userEntity.js";
 
 export class UserRepository{
     /**
      * Cria um usuário no banco de dados.
-     * @param user Um CreateUserDTO com os dados do usuário a ser criado.
-     * @returns O objeto do usuário criado sem a sua senha.
+     * @param user Um objeto User com os dados do usuário a ser criado.
+     * @returns O objeto do usuário criado.
      */
-    public async create(user:CreateUserDTO):Promise<SafeUserDTO>{
+    public async create(user:User):Promise<User|null>{
         await using db = await DBConnection.connect()
         const {name,email,password,ra,isAdmin} = user;
         try{
-            await db.query(
-                "INSERT INTO users (ra,email,name,pw_hash,\"isAdmin\") VALUES ($1,$2,$3,$4,$5);",
+            const rows = await db.query(
+                "INSERT INTO usuarios (ra,email,nome,senha_hash,is_admin) VALUES ($1,$2,$3,$4,$5);",
                 [ra,email,name,password,isAdmin]
             );
-            const safeUser:SafeUserDTO = {name,email,ra,isAdmin};
-            return safeUser;
+            if (rows.rowCount&&rows.rowCount>0) return user;
+            return null
         }catch(error){
-            if(error instanceof DatabaseError && error.code === "ER_DUP_ENTRY" ) {
+            if(error instanceof DatabaseError && error.code === "23505" /*unique_violation*/ ) {
                 throw new Error("O RA/Email está em uso.")
             }
-            throw new Error("Não foi possível criar usuário no BD.")
+            throw new Error("Não foi possível criar usuário no BD.\nErro: "+error)
         }
         //TODO: Verificar quantidade de dados quando banco ficar pronto.
     }
     /**
      * Busca o usuário no banco de dados pelo seu registro acadêmico.
      * @param ra string contendo o R.A. a ser buscado.
-     * @returns Um SafeUserDTO com os dados do usuário encontrado, ou null caso não o encontre.
+     * @returns Um um objeto User com os dados do usuário encontrado, ou null caso não o encontre.
      */
-    public async searchByRa(ra: string):Promise<SafeUserDTO|null>{
+    public async searchByRa(ra: string):Promise<User|null>{
         await using db = await DBConnection.connect()
         try{
-            const rows = await db.query("SELECT * FROM users WHERE ra = $1 ;",[ra]);
+            const rows = await db.query("SELECT * FROM usuarios WHERE ra = $1 ;",[ra]);
             if(rows && rows.rowCount){
-                const {name,ra,email,isAdmin} = rows.rows[0] as unknown as SafeUserDTO;
-                const safeUser:SafeUserDTO = { name, ra, email, isAdmin };
-                return safeUser;
+                const {nome,ra,email, is_admin, senha_hash} = rows.rows[0] as unknown as {nome:string,ra:string,email:string,is_admin:boolean,senha_hash:string};
+                const user = User.create(nome, ra, email, senha_hash, is_admin);
+                return user;
             }
             return null;
         }catch(error){
@@ -51,15 +52,15 @@ export class UserRepository{
     /**
      * Busca o usuário no banco de dados pelo seu e-mail.
      * @param email string contendo o email a ser buscado.
-     * @returns Um SafeUserDTO com os dados do usuário encontrado, ou null caso não o encontre.
+     * @returns Um objeto User com os dados do usuário encontrado, ou null caso não o encontre.
      */
-    public async searchByEmail(email:string):Promise<SafeUserDTO|null>{
+    public async searchByEmail(email:string):Promise<User|null>{
         await using db = await DBConnection.connect()
         try{
-            const rows = await db.query("SELECT * FROM users WHERE email = $1 ;",[email]);
-            if(rows && rows.rowCount){
-                const {name,ra,email, isAdmin} = rows.rows[0] as unknown as SafeUserDTO;
-                const safeUser:SafeUserDTO = { name, ra, email, isAdmin };
+            const rows = await db.query("SELECT * FROM usuarios WHERE email = $1 ;",[email]);
+            if(rows.rowCount && rows.rowCount>0){
+                const {nome,ra,email, is_admin, senha_hash} = rows.rows[0] as unknown as {nome:string,ra:string,email:string,is_admin:boolean,senha_hash:string};
+                const safeUser = User.create(nome, email, ra, senha_hash, is_admin);
                 return safeUser;
             }
             return null;
@@ -67,34 +68,25 @@ export class UserRepository{
             if(error instanceof DatabaseError){
                 throw new Error("Erro: "+error.message);
             }
-            throw new Error("Erro:"+error);
+            throw new Error("Erro: "+error);
         }
     }
     /**
      * Atualiza o nome de um usuário.
      * @param user Um UpdateUserNameDTO com um dado a ser buscado e o nome a ser atualizado.
-     * @returns Um SafeUserDTO com as novas informações caso de certo o update, ou null caso dê errado. 
+     * @returns Um objeto User com as novas informações caso de certo o update, ou null caso dê errado. 
      */
-    public async updateName(user:UpdateUserNameDTO):Promise<SafeUserDTO|null>{
-        const {name,email,ra} = user;
-        const toConsult = ra||email;
-        const id = ra ? 'ra' : 'email';
+    public async updateName(user:User):Promise<boolean>{
         await using db = await DBConnection.connect();
         try{
             const result = await db.query(
-                `UPDATE users SET name = $1 WHERE ${id} = $2`,
-                [name,toConsult]
+                `UPDATE usuarios SET nome = $1 WHERE ra = $2`,
+                [user.name,user.ra]
             );
             if(result.rowCount){
-                if(email){
-                    const safeUser:SafeUserDTO = await this.searchByEmail(email) as SafeUserDTO
-                    return safeUser;
-                }else if(ra){
-                    const safeUser:SafeUserDTO = await this.searchByRa(ra) as SafeUserDTO
-                    return safeUser;
-                }
+                return true;
             }
-            return null;
+            return false;
         }catch(error){
             console.error("Erro:"+error)
             throw new Error("Não foi possível atualizar nome de usuário.")
@@ -103,29 +95,19 @@ export class UserRepository{
     /**
      * Atualiza a senha de um usuário.
      * @param user Um objeto UpdateUserPasswordDTO com a senha nova a ser atualizada e o método de indentificação do usuário.
-     * @returns Um objeto SafeUserDTO caso a operação tenha sido realizada com sucesso, ou null caso não.
+     * @returns Um objeto User caso a operação tenha sido realizada com sucesso, ou null caso não.
      */
-    public async updatePassword(user:UpdateUserPasswordDTO):Promise<SafeUserDTO|null>{
-        const password = user.password;
-        const toConsult = user.email || user.ra;
-        const idMethod = user.ra ? "ra" : "email";
+    public async updatePassword(user:User):Promise<boolean>{
         await using db = await DBConnection.connect();
         try{
             const result = await db.query(
-                `UPDATE users SET password = $1 WHERE ${idMethod} = $2;`,
-                [password,toConsult]
+                `UPDATE users SET password = $1 WHERE ra = $2;`,
+                [user.password,user.ra]
             );
             if(result.rowCount){
-                if(user.email){
-                    const safeUser:SafeUserDTO = await this.searchByEmail(user.email) as SafeUserDTO;
-                    return safeUser;
-                }
-                else if(user.ra){
-                    const safeUser:SafeUserDTO = await this.searchByRa(user.ra) as SafeUserDTO;
-                    return safeUser;
-                }
+                return true;
             }
-            return null;
+            return false;
         }catch(error){
             console.error("Erro:"+error);
             throw new Error("Não foi possível atualizar a senha do usuário.");
@@ -134,32 +116,40 @@ export class UserRepository{
     /**
      * Atualiza o e-mail de um usuário.
      * @param user Um objeto UpdateUserEmailDTO com o email a ser atualizado e o método de identificação do Usuário.
-     * @returns Um objeto SafeUserDTO com as novas informações atualizadas.
+     * @returns Um objeto User com as novas informações atualizadas.
      */
-    public async updateEmail(user:UpdateUserEmailDTO):Promise<SafeUserDTO|null>{
-        const newEmail = user.newEmail;
-        const toConsult = user.oldEmail || user.ra;
-        const idMethod = user.ra ? "ra" : "email";
+    public async updateEmail(user:User):Promise<boolean>{
         await using db = await DBConnection.connect();
         try{
             const result = await db.query(
-                `UPDATE users SET email = $1 WHERE ${idMethod} = $2;`,
-                [newEmail,toConsult]
+                `UPDATE usuarios SET email = $1 WHERE ra = $2;`,
+                [user.email,user.ra]
             );
             if(result.rowCount){
-                if(user.oldEmail){
-                    const safeUser:SafeUserDTO = await this.searchByEmail(user.newEmail) as SafeUserDTO;
-                    return safeUser;
-                }
-                else if(user.ra){
-                    const safeUser:SafeUserDTO = await this.searchByRa(user.ra) as SafeUserDTO;
-                    return safeUser;
-                }
+                return true;
             }
-            return null;
+            return false;
         }catch(error){
             console.error("Erro:"+error);
             throw new Error("Não foi possível atualizar o e-mail do usuário.");
+        }
+    }
+
+    public async updateRa(user:User){
+        await using db = await DBConnection.connect();
+        console.log(user)
+        try{
+            const result = await db.query(
+                `UPDATE usuarios SET ra=$1 WHERE email=$2 ;`,
+                [user.ra,user.email]
+            )
+            console.log(result)
+            if(result.rowCount){
+                return true;
+            }
+            return false;
+        }catch(error){
+            throw new Error("Erro: " + error);
         }
     }
     /**
