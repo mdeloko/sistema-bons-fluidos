d69fe0e0cf27e45592e0e8f26a1398941e2d73bb
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2025-06-17 01:35:21 -0300

    feat: criação de algumas funcionalidades iniciais do Service de Usuários.

--- backend/src/models/services/userService.ts
+++ backend/src/models/services/userService.ts
@@ -1,37 +1,151 @@
-
 import { validatePassword } from "../../utils/validations.js";
-import { CreateUserDTO, SafeUserDTO, UpdateUserEmailDTO } from "../dtos/userDtos.js";
+import {
+	CreateUserDTO,
+	SafeUserDTO,
+	SearchUserEMailDTO,
+	SearchUserRaDTO,
+	UpdateUserEmailDTO,
+	UpdateUserNameDTO,
+    UpdateUserPasswordDTO,
+    UpdateUserRaDTO,
+} from "../dtos/userDtos.js";
 import { User } from "../entities/userEntity.js";
 import { UserRepository } from "../repositories/userRepository.js";
-import {hash} from "bcrypt"
+import { hash } from "bcrypt";
 
 export class UserService {
-	constructor(private repository: UserRepository) {
-    }
-	public async create(
-		user: CreateUserDTO,
-	): Promise<SafeUserDTO | null> {
-		if (validatePassword(user.password)) {
+	constructor(private repository: UserRepository) {}
+
+	public async create(user: CreateUserDTO): Promise<SafeUserDTO | null> {
+		if (!validatePassword(user.password)) {
 			throw new Error(
 				`Senha muito curta para usuário: ${user.name} - ${user.ra}`,
 			);
 		}
-		if (await this.repository.searchByEmail(user.email)) {
+		if (
+			(await this.repository.searchByEmail(user.email)) ||
+			(await this.repository.searchByRa(user.ra))
+		) {
 			return null;
 		}
-        const hashedPw = await hash(user.password,10);
-        const newUser = await User.create(user.name,user.email,user.ra,hashedPw,false);
-        return await this.repository.create(newUser);
+		const hashedPw = await hash(user.password, 10);
+		const userEnt = await User.create(
+			user.name,
+			user.email,
+			user.ra,
+			hashedPw,
+			user.isAdmin,
+		);
+		if (await this.repository.create(userEnt)) {
+			const safeUser: SafeUserDTO = {
+				name: userEnt.name,
+				email: userEnt.email,
+				isAdmin: userEnt.isAdmin,
+				ra: userEnt.ra,
+			};
+			return safeUser;
+		}
+		return null;
 	}
-    public async updateEmail(userToUpdate:UpdateUserEmailDTO){
-        const toConsult = userToUpdate.ra? 'ra': 'email'
-        const id =  userToUpdate.oldEmail || userToUpdate.ra;
-        if(toConsult == 'ra' && id){
-            const user = await this.repository.searchByRa(id) as User;
-            if(user){
-                user.email = userToUpdate.newEmail;
-                return await this.repository.updateEmail()
-            }
-        }
-    }
+
+	public async updateEmail(
+		userToUpdate: UpdateUserEmailDTO,
+	): Promise<SafeUserDTO | null> {
+		const user = await this.repository.searchByRa(userToUpdate.ra);
+		if (user) {
+			user.email = userToUpdate.newEmail;
+			if (await this.repository.updateEmail(user)) {
+				const userDto: SafeUserDTO = {
+					name: user.name,
+					email: user.email,
+					isAdmin: user.isAdmin,
+					ra: user.ra,
+				};
+				return userDto;
+			}
+		}
+		return null;
+	}
+
+	public async updateName(userToUpdate: UpdateUserNameDTO) {
+		const user = await this.repository.searchByRa(userToUpdate.ra);
+		if (user) {
+			user.name = userToUpdate.name;
+			if (await this.repository.updateName(user)) {
+				const userDto: SafeUserDTO = {
+					name: user.name,
+					email: user.email,
+					isAdmin: user.isAdmin,
+					ra: user.ra,
+				};
+				return userDto;
+			}
+		}
+	}
+
+	public async updatePassword(
+		userToUpdate: UpdateUserPasswordDTO,
+	): Promise<SafeUserDTO | null> {
+		const user = await this.repository.searchByRa(userToUpdate.ra);
+		if (user) {
+			user.password = userToUpdate.password;
+			if (await this.repository.updatePassword(user)) {
+				const userDto: SafeUserDTO = {
+					name: user.name,
+					email: user.email,
+					isAdmin: user.isAdmin,
+					ra: user.ra,
+				};
+				return userDto;
+			}
+		}
+		return null;
+	}
+
+	public async updateRa(
+		userToUpdate: UpdateUserRaDTO,
+	): Promise<SafeUserDTO | null> {
+		const user = await this.repository.searchByEmail(userToUpdate.email);
+		if (user) {
+			user.ra = userToUpdate.newRa;
+			if (await this.repository.updateRa(user)) {
+				const userDto: SafeUserDTO = {
+					name: user.name,
+					email: user.email,
+					isAdmin: user.isAdmin,
+					ra: user.ra,
+				};
+				return userDto;
+			}
+		}
+		return null;
+	}
+
+	public async searchEmail(userToSearch: SearchUserEMailDTO) {
+		const user = await this.repository.searchByEmail(userToSearch.email);
+		if (user) {
+			const userDto: SafeUserDTO = {
+				name: user.name,
+				email: user.email,
+				isAdmin: user.isAdmin,
+				ra: user.ra,
+			};
+			return userDto;
+		}
+		return null;
+	}
+
+	public async searchRa(userToSearch: SearchUserRaDTO) {
+		const user = await this.repository.searchByRa(userToSearch.ra);
+		if (user) {
+			const userDto: SafeUserDTO = {
+				name: user.name,
+				email: user.email,
+				isAdmin: user.isAdmin,
+				ra: user.ra,
+			};
+			return userDto;
+		}
+		return null;
+	}
 }
