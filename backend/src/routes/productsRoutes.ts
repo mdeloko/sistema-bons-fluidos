e7f100a9882ef440af332f938e764059e2d3d8f1
Gian <gian.2000@hotmail.com>
2025-06-19 01:38:14 -0300

    feat: Add JWT authentication to product routes

--- backend/src/routes/productsRoutes.ts
+++ backend/src/routes/productsRoutes.ts
@@ -3,6 +3,7 @@ import { ProductController } from "../controllers/productController.js";
 import { ProductService } from "../models/services/productService.js";
 import { ProductRepository } from "../models/repositories/productRepository.js";
 import { IProductRepository } from "../models/repositories/IProductRepository.js";
+import { verifyJWT } from "../utils/validations.js"; // <--- Importe o verifyJWT
 
 export const router = Router({mergeParams: true});
 
@@ -13,11 +14,11 @@ const productController = new ProductController(productService);
 
 // Rotas de Produto
 // Usamos arrow functions para garantir que o 'this' dentro dos métodos do controller
-// aponte para a instância correta de productController.
-router.post("/", (req, res) => productController.createProduct(req, res));
-router.get("/", (req, res) => productController.getAllProducts(req, res));
-router.get("/id/:id", (req, res) => productController.getProductById(req, res));
-router.get("/name/:name", (req, res) => productController.getProductByName(req, res));
-router.get("/origin/:origin", (req, res) => productController.getProductsByOrigin(req, res));
-router.put("/:id", (req, res) => productController.updateProduct(req, res));
-router.delete("/:id", (req, res) => productController.deleteProduct(req, res));
+// Aplicando verifyJWT
+router.post("/", verifyJWT, (req, res) => productController.createProduct(req, res));
+router.get("/", verifyJWT, (req, res) => productController.getAllProducts(req, res));
+router.get("/id/:id", verifyJWT, (req, res) => productController.getProductById(req, res));
+router.get("/name/:name", verifyJWT, (req, res) => productController.getProductByName(req, res));
+router.get("/origin/:origin", verifyJWT, (req, res) => productController.getProductsByOrigin(req, res));
+router.put("/:id", verifyJWT, (req, res) => productController.updateProduct(req, res));
+router.delete("/:id", verifyJWT, (req, res) => productController.deleteProduct(req, res));
